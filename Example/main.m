98ddb3b049188d7fa8245f2c121c4102306eb126
AliSoftware <olivier@halligon.net>
2010-06-02 03:15:44 +0200

    New implementation with orientation & colorspace bug fixes + example
     - Modified the implementation : using UIGraphcsBeginImageContext() instead of CGBitmapContextCreate() --> no more problems with some odd colospace/alpha/nbComponents configurations
    
     - Now correctly supports the imageOrientation (EXIF orientation) stored in the UIImage and take it into account
    
     - Added Example project

---
+++ Example/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UIImageResizeTest
+//
+//  Created by Olivier on 28/05/10.
+//  Copyright AliSoftware 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
